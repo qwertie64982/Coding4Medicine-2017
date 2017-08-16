c4aeb1c90f2f28ff6c0e225ff7c687c298afbc19
Qwertie64982 <maxwell.a.sherman@live.com>
2017-08-16 19:41:31 +0000

    Added user input and sorting to k-mers

--- Jigsaw/k-mers.py
+++ Jigsaw/k-mers.py
@@ -1,21 +1,38 @@
 from Bio import SeqIO
-reads = list(SeqIO.parse("/home/maxwell.a.sherman/genomes/ecoli.fa", "fasta"))
-genome = str(reads[0].seq)
-# k = raw_input("Lengh of k for k-mers: ")
+import os
 
+reads = list(SeqIO.parse("/home/maxwell.a.sherman/genomes/ecoli.fa", "fasta"))
+stringInput = raw_input("Enter range of k-mer sizes, [x, y) : ")
+kSizes = map(int, stringInput.split(", "))
 kmers = {}
-for k in range(2, 13):
-    for i in range(0, len(genome) - k + 1):
-        if genome[i:i + k] not in kmers:
-            kmers[genome[i:i + k]] = 0
+
+# Find all k-mers of genome
+genome1 = str(reads[0].seq)
+for k in range(kSizes[0], kSizes[1]):
+    for i in range(0, len(genome1) - k + 1):
+        if genome1[i:i + k] not in kmers:
+            kmers[genome1[i:i + k]] = 1
         else:
-            kmers[genome[i:i + k]] += 1
+            kmers[genome1[i:i + k]] += 1
+
+# Find all k-mers of genome reverse compliment
+genome2 = str(reads[0].seq.reverse_complement())
+for k in range(kSizes[0], kSizes[1]):
+    for i in range(0, len(genome2) - k + 1):
+        if genome2[i:i + k] not in kmers:
+            kmers[genome2[i:i + k]] = 1
+        else:
+            kmers[genome2[i:i + k]] += 1
 
 print "k-mers found, writing to file."
 
 outfile = open("k-mers.txt", "w+")
-for i in range(2, 13):
+for i in range(kSizes[0], kSizes[1]):
     for kmer in kmers:
         if len(kmer) == i and kmers[kmer] > 1:
             outfile.write("%s: %d\n" % (kmer, kmers[kmer]))
-            # print "%s: %d" % (kmer, kmers[kmer])
+
+print "Successfully written to k-mers.txt."
+
+os.system("sort -k 2 -n -r k-mers.txt -o k-mers.txt")
+print "Sorted."
